9681d6d6569231780cf7a39f403ef4b52c843cc2
mor <mor@LAPTOP-JKSGA5B3>
2022-08-05 18:57:09 +0300

    first commit

---
+++ web_project/views.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+def login_user(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+    return render(request, 'registration/login.html')
+

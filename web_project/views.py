719ae06aaf1c5b02610e8cc363f41de7eab2fbcb
mor <mor@LAPTOP-JKSGA5B3>
2022-08-07 07:00:05 +0300

    create home page, header and footer

--- web_project/views.py
+++ web_project/views.py
@@ -1,7 +1,9 @@
-from django import forms
+import mimetypes
+import os;
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 def login_user(request):
     logout(request)
@@ -16,3 +18,22 @@ def login_user(request):
                 return HttpResponseRedirect('/')
     return render(request, 'registration/login.html')
 
+def download_file(request, filename=''):
+    if filename != '':
+        # Define Django project base directory
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        # Define the full file path
+        filepath = BASE_DIR + "\\files\\" + filename
+        # Open the file for reading content
+        path = open(filepath, 'rb')
+        # Set the mime type
+        mime_type, _ = mimetypes.guess_type(filepath)
+        # Set the return value of the HttpResponse
+        response = HttpResponse(path, content_type=mime_type)
+        # Set the HTTP header for sending to browser
+        response['Content-Disposition'] = "attachment; filename=%s" % filename
+        # Return the response value
+        return response
+    else:
+        # Load the template
+        return render(request, '/')
